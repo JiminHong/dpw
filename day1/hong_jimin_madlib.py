2d5e8fc79e830eddf638fe4a429943c47b67c20c
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2015-05-06 00:46:05 -0400

    commit4- pick random two colors

--- day1/hong_jimin_madlib.py
+++ day1/hong_jimin_madlib.py
@@ -3,7 +3,14 @@
 # And you will have a random number of pens of the color you picked.
 # I will tell you how many pens you will get.
 
-
 # This is a list of colors you can choose
 colors = ["red", "blue", "yellow"]
 
+# You are going to pick two random colors
+import random
+
+first_choice = random.randint(0, len(colors)-1)
+second_choice = random.randint(0, len(colors)-1)
+
+# print "Your first choice is " + colors[first_choice] +"."
+# print "Your second choice is " + colors[second_choice] +"."
