d902e93069563ddbaf510ca07fba46a618cf8bdb
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2015-05-06 00:44:44 -0400

    commit2 - description

--- day1/hong_jimin_madlib.py
+++ day1/hong_jimin_madlib.py
@@ -1,6 +1,7 @@
 # You pick two colors randomly.
+# I will mix those two colors.
 # And you will have a random number of pens of the color you picked.
-
+# I will tell you how many pens you will get.
 
 
 
