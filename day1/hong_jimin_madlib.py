939a3b8b513cb97cb98e503fbc4e5b61709d38dd
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2015-05-06 00:46:55 -0400

    commit5- mix two colors
    1. If statement
    2. def (function)
    3. two strings

--- day1/hong_jimin_madlib.py
+++ day1/hong_jimin_madlib.py
@@ -14,3 +14,18 @@ second_choice = random.randint(0, len(colors)-1)
 
 # print "Your first choice is " + colors[first_choice] +"."
 # print "Your second choice is " + colors[second_choice] +"."
+
+# I will mix two colors you picked.
+def mixing(f,s):
+    result = ""
+    if f == s :
+        result += s
+    elif f == "red" and s == "blue" or f == "blue" and s == "red":
+        result += "violet"
+    elif f == "blue" and s == "yellow" or f == "yellow" and s == "blue":
+        result += "green"
+    elif f == "red" and s == "yellow" or f == "yellow" and s == "red":
+        result += "orange"
+    print "You picked " + f + " and " + s + ". I mixed those two colors and got " + result + " color."
+
+mixing(colors[first_choice], colors[second_choice])
