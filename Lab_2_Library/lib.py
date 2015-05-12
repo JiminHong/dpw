96593eb52d96fe0b7c57595a464395da62b4ed6f
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2015-05-12 02:11:30 -0400

    delete

--- Lab_2_Library/lib.py
+++ Lab_2_Library/lib.py
@@ -1,18 +1,12 @@
 class Print:
     def __init__(self):
-        print "Print created"
-    # def print_grade(self, g):
-    #     print "Grade : " + str(g)
-    # def print_total_hours(self, h):
-    #     print "Total hours is : " + str(h)
-    # def print_name(self, n):
-    #     print "Your name is : " + n
+        pass
     def print_result(self, g, h, n, w, s):
         print str(n) + ", you got " + str(g) + " on your final exam, therefore your wage per an hour is $" + str(w) + ". Since you work " + str(h) + " hour(s) a day, your monthly salary is " + str(s) + "."
 
 class Utilities:
     def __init__(self):
-        print "Utilites created"
+        pass
     def get_wage(self, grade):
         if int(grade) >= 80:
             wage = 30
@@ -22,5 +16,4 @@ class Utilities:
             wage = 10
         return wage
     def get_salary(self, hours, wage):
-        total = int(hours) * 5 * 4 * int(wage)
-        return total
+        return int(hours) * 5 * 4 * int(wage)
