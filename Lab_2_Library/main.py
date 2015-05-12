847b8e6fdce8a6e01784d9f4ed975e62b498e241
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2015-05-11 21:50:32 -0400

    description

--- Lab_2_Library/main.py
+++ Lab_2_Library/main.py
@@ -1,1 +1,8 @@
-__author__ = 'jiminhong'
+# Description
+# 1. You will enter your name.
+# 2. You will enter your grades of two exams.
+# 3. You will enter how many hours you work daily.
+# I will show you your average grade and your monthly salary.
+# Same or Higher than 80 => $30/hour
+# Same or Higher than 60 and lower than 79 => $20/hour
+# Lower than 60 => $10/hour
